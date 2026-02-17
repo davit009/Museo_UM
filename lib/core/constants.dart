dd774caa3e5f5ffee2dea178e3af08b4db744a3b
davit009 <127544786+davit009@users.noreply.github.com>
2026-02-17 16:57:44 -0600

    credenciales

---
+++ lib/core/constants.dart
@@ -0,0 +1,5 @@
+
+  class SupabaseConstants {
+  static const String url = 'https://ybnuczqimbjzyotjdgsx.supabase.co';
+  static const String anonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlibnVjenFpbWJqenlvdGpkZ3N4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEzNjU0OTMsImV4cCI6MjA4Njk0MTQ5M30.OsSVLF7VfjSiD5AkPQKi6hMNzeUit6VY6E_Z9OUIuNI';
+}
